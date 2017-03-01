e9abd8836544f02b7c1fcd60a67fc38b1e0916bc
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2017-03-01 08:27:22 -0300

    proyecto inicial base

--- Independiente/App_Start/BundleConfig.cs
+++ Independiente/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace Independiente
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/mi-estilo.css"));
         }
     }
 }
